4cecc9090bae3a77ab2d37e4e1f80eae8cce6e57
Gabriela <gabrielacamargosanguino@gmail.com>
2024-08-04 23:25:55 -0500

    First advance

---
+++ my-app/app/components/IntroButtons.tsx
@@ -0,0 +1,35 @@
+import { RiBarChart2Fill } from "react-icons/ri";
+import { RiSettings5Fill } from "react-icons/ri";
+import { HiMiniUserCircle } from "react-icons/hi2";
+import { ImMenu } from "react-icons/im";
+
+
+export default function IntroButtons() {
+    return (
+        <>
+            <RiBarChart2Fill />
+            <button type="button"
+                className="Rounded-lg text-sm pr-2 pl-1 py-3 text-white">
+                Report
+            </button>
+
+            <RiSettings5Fill />
+            <button type="button"
+                className="Rounded-lg text-sm pr-2 pl-1 py-3 text-white">
+                Setting
+            </button>
+
+            <HiMiniUserCircle />
+            <button type="button"
+                className="Rounded-lg text-sm pr-2 pl-1 py-3 text-white">
+                Sign in
+            </button>
+
+            <button type="button"
+                className="Rounded-lg text-sm pr-2 pl-1 py-3 text-white">
+                <ImMenu />
+            </button>
+
+        </>
+    )
+}
